83ae64b173c53726ab88f8fc80b3a582bb5ab564
Somaiya-0 <sdark.babe@gmail.com>
2026-07-30 05:20:40 +0600

    aa

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("DJANGO_SECRET_KEY", default="dev-insecure-key-change-me")
 DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost,127.0.0.1,34.239.101.23,zoning-reflected-prerequisite-fare.trycloudflare.com", cast=Csv())
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost,127.0.0.1,34.239.101.23,longest-neighborhood-port-sustained.trycloudflare.com", cast=Csv())
 
 INSTALLED_APPS = [
     "django.contrib.admin",
